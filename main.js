e67cba403560cd69f7e2d7a96f52276c30a0170b
Cheryl Brown <38012775+sousou617@users.noreply.github.com>
2018-04-25 15:59:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// function printInt(n){
+// 	var answer = 0;
+// 	for(var i=1; i<=10; i=i+1){
+// 		// answer=answer + 1;
+// 	}
+// 	// var i = n
+// 	// return answer;
+// 	console.log(printInt(10))
+// }
+
+var i=1
+while (i<=10) {
+	document.write('number '+ i + '<br/>');
+	i=i+1
+}
+
+
+
+
